f72bb28c080aa471e157dc68d82ced410473e842
balzikz <alikkhhhannnn@gmail.com>
2025-09-01 08:55:06 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/df-mc/dragonfly/server"
+	"github.com/df-mc/dragonfly/server/player"
+	"github.com/df-mc/dragonfly/server/player/chat"
 	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 func main() {
@@ -18,6 +22,30 @@ func main() {
 	srv := server.New(&conf, log)
 	srv.Start()
 
-	for srv.Accept(nil) {
+	for srv.Accept(func(p *player.Player) {
+		p.Message("§eДобро пожаловать на наш Dragonfly сервер!")
+		p.Handle(NewPlayerHandler(p))
+	}) {
 	}
+}
+
+type PlayerHandler struct {
+	player.NopHandler
+	p                 *player.Player
+}
+
+
+func NewPlayerHandler(p *player.Player) *PlayerHandler {
+	return &PlayerHandler{p: p}
+}
+
+func (h *PlayerHandler) HandleCommandExecution(commandLine *string) {
+	command := strings.Split(*commandLine, " ")[0]
+
+	if command == "heal" {
+		h.p.Heal(1000, "Исцеление командой /heal")
+		h.p.Message("§aВы были полностью исцелены!")
+		return
+ }
+	h.p.Messagef("§cКоманда '%s' не найдена.", command)
 }
